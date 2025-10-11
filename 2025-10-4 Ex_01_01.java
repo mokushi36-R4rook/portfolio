73cfdd16740dd351d74dabdb1d9e26fa88b78cd7
mokushi36-R4rook <mokushi36@gmail.com>
2025-10-11 22:29:59 +0900

    Create 2025-10-4 Ex_01_01.java

---
+++ 2025-10-4 Ex_01_01.java
@@ -0,0 +1,16 @@
+/*制作日　2025/10/04
+
+プログラムの目的
+課題が要求している文字列をその指定通りに出力する
+
+プログラムの解説
+printlnを用いて要求された文字列を標準出力に出力する。*/
+``` java
+import java.util.*;
+
+public class Ex_01_01{
+    public static void main(String[] args){
+        System.out.println("Tokyo Online University");
+        System.out.println("Java programming");
+    }
+}
