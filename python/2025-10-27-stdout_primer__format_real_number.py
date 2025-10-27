f86d9e1bc9f9c619c4342cb0316d69ea4a9d1474
mokushi36-R4rook <mokushi36@gmail.com>
2025-10-27 14:33:56 +0900

    Create 2025-10-27-stdout_primer__format_real_number.py
    入力として受け取った複数の実数を、小数点以下の出力するけた数を指定して出力

---
+++ python/2025-10-27-stdout_primer__format_real_number.py
@@ -0,0 +1,8 @@
+#入力として受け取った複数の実数を、小数点以下の出力するけた数を指定して出力
+t = int(input())
+num = [input().split() for _ in range(t)]
+
+for i in range(t):
+    a = float(num[i][0])
+    b = int(num[i][1])
+    print("{:.{}f}".format(a, b))
