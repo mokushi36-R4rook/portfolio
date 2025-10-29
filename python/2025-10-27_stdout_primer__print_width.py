8983b98f6b309c04d7de8f78e8ac8c201b0a7e7f
mokushi36-R4rook <mokushi36@gmail.com>
2025-10-29 12:33:33 +0900

    Update 2025-10-27_stdout_primer__print_width.py

--- python/2025-10-27_stdout_primer__print_width.py
+++ python/2025-10-27_stdout_primer__print_width.py
@@ -1,8 +1,9 @@
-#N 個の整数を M けたになるよう、半角スペース埋めで出力
-i = input().split()
-t = int(i[0])
-k = int(i[1])
-num = [input() for _ in range(t)]
-
-for j in num:
-    print("{:{}d}".format(int(j), k))
+def main():
+    #N 個の整数を M けたになるよう、半角スペース埋めで出力
+    i = input().split()
+    t = int(i[0])
+    k = int(i[1])
+    num = [input() for _ in range(t)]
+    
+    for j in num:
+        print("{:{}d}".format(int(j), k))
