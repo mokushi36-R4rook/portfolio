c179ae9ff23b8690fa4dcd304574b1d4669bed7d
mokushi36-R4rook <mokushi36@gmail.com>
2025-10-29 12:34:06 +0900

    Update 2025-10-27-stdout_primer__format_real_number.py

--- python/2025-10-27-stdout_primer__format_real_number.py
+++ python/2025-10-27-stdout_primer__format_real_number.py
@@ -1,8 +1,9 @@
-#入力として受け取った複数の実数を、小数点以下の出力するけた数を指定して出力
-t = int(input())
-num = [input().split() for _ in range(t)]
-
-for i in range(t):
-    a = float(num[i][0])
-    b = int(num[i][1])
-    print("{:.{}f}".format(a, b))
+    def main():
+        #入力として受け取った複数の実数を、小数点以下の出力するけた数を指定して出力
+        t = int(input())
+        num = [input().split() for _ in range(t)]
+        
+        for i in range(t):
+            a = float(num[i][0])
+            b = int(num[i][1])
+            print("{:.{}f}".format(a, b))
